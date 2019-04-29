46c3dcec90e8998de327c6c8ede095d5d7037cea
SamAgua <31320434+SamAgua@users.noreply.github.com>
2019-04-29 00:41:01 -0500

    update stitch.cpp
    stitches two images

--- stitch.cpp
+++ stitch.cpp
@@ -11,13 +11,15 @@ using namespace cv;
 using namespace cv::xfeatures2d;
 using std::cout;
 using std::endl;
+
 int main(int argc, char* argv[])
 {
-
+	const double nn_match_ratio = 0.8f; // Nearest-neighbour matching ratio
 	Mat src;
 	Mat src2;
 	src = imread("iqbal1.png", IMREAD_COLOR);
 	src2 = imread("iqbal2.png", IMREAD_COLOR);
+
 	if (src.empty())
 	{
 		cout << "Could not open or find the image!\n" << endl;
@@ -40,27 +42,55 @@ int main(int argc, char* argv[])
 		std::vector< std::vector<DMatch> > knn_matches;
 		matcher->knnMatch(desc1, desc2, knn_matches, 2);
 		//-- Filter matches using the Lowe's ratio test
-		const float ratio_thresh = 0.7f;
-		std::vector<DMatch> good_matches;
-		for (size_t i = 0; i < knn_matches.size(); i++)
-		{
-			if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
-			{
-				good_matches.push_back(knn_matches[i][0]);
+	
+		//std::vector< std::vector<DMatch> > good_matches;
+		std::vector<KeyPoint> matched1, matched2;
+		for (unsigned i = 0; i < knn_matches.size(); i++) {
+			if (knn_matches[i][0].distance < nn_match_ratio * knn_matches[i][1].distance) {
+				matched1.push_back(keypoints1[knn_matches[i][0].queryIdx]);
+				matched2.push_back(keypoints2[knn_matches[i][0].trainIdx]);
 			}
 		}
+		int max1 = 0;
+		int max2 = 0;
+		std::vector<KeyPoint>::const_iterator it = matched1.begin(), end = matched1.end();
+		for (; it != end; ++it) {
+			if (it->pt.x > max1) {
+				max1 = it->pt.x;
+			}
+		}
+		std::vector<KeyPoint>::const_iterator it2 = matched2.begin(), end2 = matched2.end();
+		for (; it2 != end2; ++it2) {
+			if (it2->pt.x > max2) {
+				max2 = it2->pt.x;
+			}
+		}
+		int left = max1 / matched1.size();
+		int right = max2 / matched1.size();
 
-		Mat img_matches;
-		drawMatches(src, keypoints1, src2, keypoints2, good_matches, img_matches, Scalar::all(-1),
-			Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::DEFAULT);
+		std::cout << max1 <<" " << max2 << std::endl;
+
+		int h = 600;
+		Mat test = cv::Mat::zeros(cv::Size(340*2, h), CV_8UC3);
+		Mat small1 = cv::Mat(src, cv::Rect(0, 0, max1, src.rows));
+		resize(small1, small1, cv::Size(340, h),0,0);
+		//cv::Rect myROI(max2, 0, src2.cols, src2.rows);
+		
+		Mat small2 = src2(Rect(max2, 0, src2.cols - max2, src2.rows));
+		resize(small2, small2, cv::Size(340, h),0,0);
+
+		small1.copyTo(test(Rect(0, 0, small1.cols, h)));
+		small2.copyTo(test(Rect(small1.cols, 0, small2.cols, small2.rows)));
+		//drawMatches(src, matched1, src2, matched2, knn_matches, img_matches, Scalar(255,0,0),
+		//	Scalar(255, 0, 0));
 		//-- Show detected matches
 		
-
+		
 		for (;;) {
-			imshow("Good Matches", img_matches);
+			imshow("Good Matches", test);
 
 			char key = waitKey(0);
-			if (char(key) == 27/*ESC*/)
+			if (char(key) == 27)
 				break;
 		}
 	return 0;
