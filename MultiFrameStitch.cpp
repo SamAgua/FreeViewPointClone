c941a640b01884a0ed1aba8603ce7e8c8c1cb7ba
noremac9 <31320431+noremac9@users.noreply.github.com>
2019-05-03 22:25:04 -0500

    added pics

--- MultiFrameStitch.cpp
+++ MultiFrameStitch.cpp
@@ -19,16 +19,18 @@ using namespace std;
 int main() {
 	//VideoCapture capture(0);
 	//VideoCapture capture2(1);
-	std::vector<Mat> imgs(3);
+	std::vector<Mat> imgs(5);
 	Mat pic1 = imread("pic1.jpg", IMREAD_COLOR);
 	Mat pic2 = imread("pic2.jpg", IMREAD_COLOR);
 	Mat pic3 = imread("pic3.jpg", IMREAD_COLOR);
+	Mat pic4 = imread("pic4.jpg", IMREAD_COLOR);
+	Mat pic5 = imread("pic5.jpg", IMREAD_COLOR);
 	Mat3b result;
 	
 	Ptr<Stitcher> stitcher = Stitcher::create();
 	
 	
-			imgs = { pic1, pic2, pic3 };
+			imgs = { pic1, pic2, pic3, pic4, pic5 };
 			Stitcher::Status status = stitcher->stitch(imgs, result);
 			if (!result.empty()) {
 				resize(result, result, Size(1080, 720), 0, 0);
